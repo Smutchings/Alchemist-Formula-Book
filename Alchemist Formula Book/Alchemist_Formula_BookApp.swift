226d0bf568a2780d7d2c3aebe624dbb95b28aa6a
Sam Hutchings <sam.hutchings@icloud.com>
2022-03-02 14:49:42 +0000

    Initial Commit

---
+++ Alchemist Formula Book/Alchemist_Formula_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alchemist_Formula_BookApp.swift
+//  Alchemist Formula Book
+//
+//  Created by Sam Hutchings on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Alchemist_Formula_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alchemist Formula Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alchemist Formula Book
+//
+//  Created by Sam Hutchings on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
